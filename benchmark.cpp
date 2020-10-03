9a2ef6c1584a15c1e88d899325af9ab71eb8a4cf
Taspon Gonggiatgul <tagonggiatgul@ucdavis.edu>
2020-10-03 15:13:36 -0700

    Updated benchmark code

--- benchmark.cpp
+++ benchmark.cpp
@@ -10,50 +10,27 @@ int main() {
     freopen ("./result/benchmark_result.txt","w",stdout);
 
     cout << "------------------------TSPLIB------------------------------" << endl;
-    string TSPLIB[] = {"rbg050c.sop","prob.42.sop"};
+    string TSPLIB[] = {"ft53.4.sop","ft70.4.sop","rbg050c.sop","rbg109a.sop","rbg150a.sop","rbg174a.sop","prob.42.sop","ry48p.4.sop"};
 
-    for (int i = 0; i < 2; i++) {
-
-        if (i == 0) {
+    for (int i = 0; i < 8; i++) {
             cout << TSPLIB[i] << " ";
-            string command_third = "./main_searchorder ./tsplib/" + TSPLIB[i] + " 7200 1237935 1";
-            string command_fourth = "./main_sequential ./tsplib/" + TSPLIB[i] + " 7200 1237935";
-            cout << endl;
-            system(command_third.c_str());
-            system(command_fourth.c_str());
-            cout << endl;
-        }
-        else {
-            cout << TSPLIB[i] << " ";
-            string command_first = "./sop_solver ./tsplib/" + TSPLIB[i]+" NN 100";
-            string command_second = "./sop_solver ./tsplib/" + TSPLIB[i]+" DH 100";
-            string command_third = "./main_searchorder ./tsplib/" + TSPLIB[i] + " 7200 1237935 1";
-            string command_fourth = "./main_sequential ./tsplib/" + TSPLIB[i] + " 7200 1237935";
+            string command_first = "./sop_solver ./tsplib/" + TSPLIB[i];
             cout << endl;
             system(command_first.c_str());
-            system(command_second.c_str());
-            system(command_third.c_str());
-            system(command_fourth.c_str());
             cout << endl;
-        }
-        
     }
 
     cout << "------------------------SOPLIB---------------------------" << endl;
 
-    string SOPLIB[] = {"R.200.1000.15.sop","R.700.100.30.sop","R.700.1000.30.sop"};
+    string SOPLIB[] = {"R.200.100.1.sop","R.200.100.15.sop","R.200.1000.1.sop","R.200.1000.15.sop","R.300.100.1.sop","R.300.100.15.sop","R.300.1000.1.sop",
+                        "R.300.1000.15.sop","R.300.1000.30.sop","R.400.100.1.sop","R.400.100.30.sop","R.400.1000.30.sop","R.500.100.1.sop","R.500.100.30.sop",
+                        "R.500.1000.30.sop","R.600.100.1.sop","R.600.100.30.sop","R.600.1000.30.sop","R.700.100.1.sop","R.700.100.30.sop","R.700.1000.30.sop"};
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 21; i++) {
         cout << SOPLIB[i] << " ";
-        string command_first = "./sop_solver ./soplib/" + SOPLIB[i]+" NN 100";
-        string command_second = "./sop_solver ./soplib/" + SOPLIB[i]+" DH 100";
-        string command_third = "./main_searchorder ./soplib/" + SOPLIB[i] + " 7200 1237935 1";
-        string command_fourth = "./main_sequential ./soplib/" + SOPLIB[i] + " 7200 1237935";
+        string command_first = "./sop_solver ./soplib/" + SOPLIB[i];
         cout << endl;
         system(command_first.c_str());
-        system(command_second.c_str());
-        system(command_third.c_str());
-        system(command_fourth.c_str());
         cout << endl;
     }
 
